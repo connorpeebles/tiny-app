ece1e55b97bdf41532aeaf005981d1914d9d1bc0
Connor Peebles <connorpeebles93@gmail.com>
2018-10-03 18:56:59 +0000

    added a users database, with four current users

--- express_server.js
+++ express_server.js
@@ -15,6 +15,29 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  "connorlol": {
+    id: "connorlol",
+    email: "connor@example.com",
+    password: "omglol"
+  },
+  "tswift13": {
+    id: "tswift13",
+    email: "tswizzle@example.com",
+    password: "iheartcats"
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
